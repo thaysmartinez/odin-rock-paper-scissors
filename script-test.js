10172afa79d00fd634e90da8a1869ffd30794d56
Thays Martinez <thaysmartinez@gmail.com>
2023-05-04 14:22:23 +0200

    Add styles to running scores

--- script-test.js
+++ script-test.js
@@ -3,9 +3,10 @@ const rpsButtons = document.querySelectorAll("button");
 const divScore = document.querySelector(".running-score");
 const spanPlayer = document.querySelector(".score-player");
 const spanComputer = document.querySelector(".score-computer");
+const choices = document.querySelector(".choices");
+const paraElement = document.createElement("p");
 
 function appendHTMLElement(result) {
-  const paraElement = document.createElement("p");
   divScore.appendChild(paraElement);
   paraElement.textContent = result;
 }
@@ -22,6 +23,8 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
   let roundDisplay;
   let roundResult;
+
+  console.log(playerSelection, computerSelection);
 
   if (playerSelection === computerSelection) {
     roundDisplay = "it's a tie!";
@@ -42,30 +45,35 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-  let userScore = 0;
+  let playerScore = 0;
   let computerScore = 0;
   let roundWinner;
   let gameWinner;
 
-  spanPlayer.textContent = userScore;
-  spanComputer.textContent = computerScore;
+  spanPlayer.textContent = `Player: ${playerScore}`;
+  spanComputer.textContent = `Computer: ${computerScore}`;
 
   rpsButtons.forEach((button) => {
     button.addEventListener("click", () => {
       roundWinner = playRound(button.dataset.choice, getComputerChoice());
       if (roundWinner === "player") {
-        userScore++;
-        spanPlayer.textContent = userScore;
+        playerScore++;
+        spanPlayer.textContent = `Player: ${playerScore}`;
       } else if (roundWinner === "computer") {
         computerScore++;
-        spanComputer.textContent = computerScore;
+        spanComputer.textContent = `Computer: ${computerScore}`;
       }
 
-      console.log(userScore, computerScore);
-      if (userScore === 5 || computerScore === 5) {
-        userScore > computerScore
+      console.log(playerScore, computerScore);
+
+      if (playerScore === 5 || computerScore === 5) {
+        playerScore > computerScore
           ? (gameWinner = "player")
           : (gameWinner = "computer");
+        choices.removeChild(spanComputer);
+        choices.removeChild(spanPlayer);
+        playerScore = 0;
+        computerScore = 0;
         appendHTMLElement(`FINAL RESULT: ${gameWinner} wins!`);
         return;
       }
